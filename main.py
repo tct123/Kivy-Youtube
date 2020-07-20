046d5e2877b1edeaa4d0c16691582523f4034a86
phuneral <colledbe@gmail.com>
2020-07-20 16:13:31 +0300

    Переделаный первый дизайн. Функция кнопки автономно работает, но Screen не поддерживает ее

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
-
-from kivymd.app import MDApp
 from kivy.config import Config
 
-Config.set('graphics', 'width', '720')
-Config.set('graphics', 'height', '1280')
+Config.set('graphics', 'width', '360')
+Config.set('graphics', 'height', '640')
 Config.set('graphics', 'resizable', False)
 
+from kivymd.app import MDApp
 
-KV = '''
+from kivy.uix.boxlayout import BoxLayout
+from kivy.lang.builder import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty  # <--- Object Property
+
+screen_helper = """
 <ContentNavigationDrawer>:
 
     ScrollView:
@@ -18,110 +19,111 @@ KV = '''
         MDList:
 
             OneLineListItem:
-                text: "Screen 1"
+                text: "Аудио"
                 on_press:
                     root.nav_drawer.set_state("close")
-                    root.screen_manager.current = "screen1"
+                    root.screen_manager.current = "menu"
 
             OneLineListItem:
-                text: "Screen 2"
+                right_icon: 'folder'
+                text: "Библиотека"
                 on_press:
                     root.nav_drawer.set_state("close")
-                    root.screen_manager.current = "screen2"
-
+                    root.screen_manager.current = "profile"
+                    
 Screen:
-    MDBottomAppBar:
-        MDToolbar:
-            id: toolbar
-            icon: "youtube"
-            type: "bottom"
-            elevation: 10
-            title: "AudioTube"
-            right_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
-            mode: 'free-center'
-            md_bg_color: .5, .5, .2, 1
-            on_action_button: [["menu", lambda x: nav_drawer.set_state("open")]]
-
-
+    MDToolbar:
+        id: toolbar
+        pos_hint: {"bottom": 1}
+        elevation: 10
+        title: "AudioTube"
+        right_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
+        
     NavigationLayout:
         x: toolbar.height
+    MainScreenManager: # <--- changes here
+        id: screen_manager
+        label_: label_welcome
+        input_name: input_name
+    
+    
+        MenuScreen:
+            name: 'menu'
+            
+            MDLabel: 
+                text: "Конверетер в аудио по ссылке"
+                pos_hint:{'center_x':0.5,'center_y':0.6}
+                halign: "center"
+                text_color: 0, 0, 1, 1
+            
+            MDTextFieldRound:
+                id: input_name
+                hint_text: 'Ваша ссылка'
+                pos_hint:{'center_x':0.5,'center_y':0.5}
+                color_mode:'custom'
+                line_color_focus: 0.43, 0.82, 0.83, 0.6
+                size_hint: .4, .05
+    
+            MDRectangleFlatButton:
+                text: 'Скачать'
+                pos_hint: {'center_x':0.5,'center_y':0.3}
+                on_press: 
+                    root.ButtonFunction()
+        ProfileScreen:
+            name: 'profile'
+            MDLabel:
+                id: label_welcome
+                text: "Hello,"
+                halign: 'center'
+            MDRectangleFlatButton:
+                text: 'Назад'
+                pos_hint: {'center_x':0.5,'center_y':0.1}
+                on_press: root.current = 'menu'
+        
+    MDNavigationDrawer:
+        id: nav_drawer
 
-        ScreenManager:
-            id: screen_manager
+        ContentNavigationDrawer:
+            screen_manager: screen_manager
+            nav_drawer: nav_drawer
 
-            Screen:
-                name: "screen1"
-                id: "screen1"
-                text: "Библиотека"
-
-                MDLabel:
-                    text: "Библиотека"
-                    icon: "book"
-                    halign: "center"
-
-            Screen:
-                name: "screen2"
-                id: "screen2"
-                text: "Конвертер аудио"
-
-
-                MDLabel:
-                    text: "Конверетер в аудио по ссылке"
-                    halign:"center"
-                    text_color: 0, 0, 1, 1
-
-                MDTextFieldRound:
-                    id: "first_field"
-                    hint_text: 'Ваша cсылка'
-                    color_mode: 'accent'
-                    size_hint: .4, .05
-                    pos_hint: {"center_x": .5, "center_y": .45}
-                    height: "400dp"
-                    helper_text_mode: "on_focus"
-                    line_color_focus: (1,1,1,1)
-
-                MDRectangleFlatButton:
-                    text: "Скачать"
-                    text_color: 0, 0, 1, 1
-                    md_bg_color: 1, 1, 1, 1 
-                    pos_hint: {'center_x': 0.5, "center_y": .3}
-                    on_release: root.check_data_text()
-
-
-        MDNavigationDrawer:
-            id: nav_drawer
-
-            ContentNavigationDrawer:
-                screen_manager: screen_manager
-                nav_drawer: nav_drawer
-
-
-'''
+"""
+class ContentNavigationDrawer(BoxLayout):
+    screen_manager = ObjectProperty ( )
+    nav_drawer = ObjectProperty ( )
 
 
-class ContentNavigationDrawer (BoxLayout):
-    screen_manager = ObjectProperty()
-    nav_drawer = ObjectProperty()
+class MainScreenManager (ScreenManager):  # This gonna be the root widget
+    label_ = ObjectProperty(None)
+    input_name = ObjectProperty(None)
 
-    def check_data_text(self):
-        text = self.scr_mngr.screen2.first_field.text
-
-        print(text)
-
-        if text == "https":
-            self.change_screen("screen1")
-
-    def change_screen(self, screen, *args):
-        self.screen_manager.current = screen
+    def ButtonFunction(self):
+        self.current = 'profile'
+        print(self.input_name.text)
+        self.label_.text = 'Скачано'
 
 
-class MyApp (MDApp):
+class MenuScreen (Screen):
+    pass
 
+
+class ProfileScreen (Screen):
+    pass
+
+
+# Create the screen manager
+sm = MainScreenManager()
+sm.add_widget(MenuScreen(name='menu'))
+
+sm.add_widget(ProfileScreen(name='profile'))
+
+
+class DemoApp (MDApp):
     def build(self):
         self.theme_cls.primary_palette = "Indigo"
         self.theme_cls.primary_hue = "300"
-        return Builder.load_string(KV)
+        screen = Builder.load_string(screen_helper)
+        return screen
 
 
-
-MyApp().run()
+DemoApp().run()
